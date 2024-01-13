f7bd232ceef2008fd34aa99c847a707965fd4a7d
Danielr-1998 <daniel.riano@utp.edu.co>
2024-01-12 21:12:25 -0500

    modificados:     src/app/pages/labs/labs.component.css 	modificados:     src/app/pages/labs/labs.component.html 	modificados:     src/app/pages/labs/labs.component.ts

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -10,14 +10,19 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './labs.component.css'
 })
 export class LabsComponent {
- 
+  tasks=['Valor 1','Valor 2','Valor 3','Valor 4','Valor 5'];
+  name ='name';
+  valor='valor';
 
   person ={
-    name: 'Daniel',
+    name: 'name',
     age:25
   }
 
   clickHandler(){
-    alert('Hola')
+    alert('alert')
+  }
+  cHandler(event:Event){
+    console.log(event);
   }
 }
