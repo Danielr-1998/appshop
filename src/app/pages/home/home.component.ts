5b3ea34b5cc4ab101d2941272d8547a6c7ddc23d
Danielr-1998 <daniel.riano@utp.edu.co>
2024-01-14 16:46:23 -0500

    nuevos archivos: src/app/models/task.model.ts 	modificados:     src/app/pages/home/home.component.html 	modificados:     src/app/pages/home/home.component.ts

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ConstantPool } from '@angular/compiler';
+
+import { task } from './../../models/task.model';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  tasks= signal<task[]>([
+    {
+      id: Date.now(),
+      title: 'Titulo Objeto',
+      completed: false
+    },
+    {
+      id: Date.now(),
+      title: 'Primer Objeto',
+      completed: false
+    }
+  ]);
 
+  addTask(title:string){
+    const newTask={
+      id: Date.now(),
+      title,
+      completed:false,
+    }
+  }
+
+  changeHandler(event:Event){
+    const input=event.target as HTMLInputElement;
+    const newTask= input.value;
+    this.tasks.update((tasks) => [...tasks,newTask]);
+  }
+
+  deletetask(index: number){
+    this.tasks.update((tasks)=> tasks.filter((tasks,position)=> position !== index));
+  }
 }
